7b7d4a6ec63a3a30b83306de19951e7197ed38f9
WenkaiQiu <107879837+wqiu1118@users.noreply.github.com>
2022-06-20 19:03:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Input 
+Input is the way the computer gets information from the user 
+
+Note: Python initially treats input as a `string` type
+
+Ex. 
+
+5 --> "5"
+
+Today is Monday --> "Today is Monday"
+
+### Formula: 
+`VariableName = input("PROMPT")`
+
+PROMPT - a question or statement that the computer asks the user 
+
+Python stores the __input__ in the __variableName__
---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+userName = input("Enter your name: ")
+#print ("Hey " + userName)
+
+userGrade = input("What grade are you in? ")
+print("Hey " + userName + ", " + userGrade + " is a lame grade")
+
+userYear = int(input("What year were you born? "))
+userAge = 2022- userYear
+print(str(userAge) + " is so young!") 
+'''
+
+
+# Create your own Madlibs 
+# Have at least 5 inputs
+
+print("Hello, I am the computer and I will assist you through this")
+name = input("Give me a name: ")
+noun = input( "Give me a noun: ")
+verb = input("Give me a verb: ")
+job = input ("Give me a job that you want to know more about: ")
+noun2 = input ("Give me another noun that: ")
+food = input ("Give me your favorite food: " )
+
+
+
+
+
+
+
+print("In the year 2069, the world has been take by the robot and humans are into hiding")
+print("You are the only human alive who controls the key to turn all the robot off. You will be accompanied ")
