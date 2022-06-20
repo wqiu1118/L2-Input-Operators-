854ec7d1494e6bca97f451d8265913f21f7186d8
WenkaiQiu <107879837+wqiu1118@users.noreply.github.com>
2022-06-20 20:10:04 +0000

    code

--- main.py
+++ main.py
@@ -16,17 +16,15 @@ print(str(userAge) + " is so young!")
 
 print("Hello, I am the computer and I will assist you through this")
 name = input("Give me a name: ")
-noun = input( "Give me a noun: ")
-verb = input("Give me a verb: ")
-job = input ("Give me a job that you want to know more about: ")
-noun2 = input ("Give me another noun that: ")
+verb = input("Give me a verb (in past tense): ")
+animal = input ("Give me an animal: ")
 food = input ("Give me your favorite food: " )
+bodypart = input ("Give me a body part: ")
 
 
 
 
-
-
-
-print("In the year 2069, the world has been take by the robot and humans are into hiding")
-print("You are the only human alive who controls the key to turn all the robot off. You will be accompanied by ")
+print("In the year 2069, the world has been take by robots and humans are into hiding")
+print("You are the only human alive who controls the key to turn all the robots off. You will be accompanied by " + name + ", and you must bypass all the challenges in order to put the key into the centerpeice of the robot mastermind in its " + bodypart + "." )
+print ("Before you start your jounry, you grabbed your favorite food, the " + food + " thinking that it might help you along the way.") 
+print("After weeks of walking, you and your assistant has found the base where the robot mastermind is. Just when you are about to open the door, " + name + " found a little " + animal + " lying on the ground. You thought it was smart enough to help, so you decided to take it with you into the base. But then it took your " +food + " and bumped into the door that just closed. I guess its not too smart after all. You got up to the top floor and started battling the mastermind while " + name + " trys to stick the key in. When he found the place to put the key in, he realizes that you still have the key. He shouts to you and signals you to throw it to him. You " + verb + " to him and tripped on a wire. That wire happened to be the power source to the mastermind and it unplugged from the wall. You have saved the world. yay!"  )
